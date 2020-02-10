d5c730b99c5b97b00bea8f3de194df010e3e9d13
mong <shdq123@gmail.com>
2020-02-11 00:01:11 +0900

    test

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const next = require('next');
 const path = require('path');
 
+const dev = process.env.NODE_ENV !== 'production';
+const prod = process.env.NODE_ENV === 'production';
+
 const server = express();
 
 server.use('/',express.static(path.join(__dirname,'public')));
 
-server.listen(3000,()=>{
+server.listen(prod ? process.env.PORT : 3000,()=>{
     console.log("server on");
 })

7e89e0eb87ec946a49dbdfd265ddfa1b7389f945
mong <shdq123@gmail.com>
2020-02-10 23:37:07 +0900

    server update

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const next = require('next');
+const path = require('path');
+
+const server = express();
+
+server.use('/',express.static(path.join(__dirname,'public')));
+
+server.listen(3000,()=>{
+    console.log("server on");
+})

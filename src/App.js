5fde7929d2f1ac2beb7518abae76ad6bedc90b28
mong <shdq123@gmail.com>
2020-02-12 22:31:46 +0900

    Remove React Starting Page & Adding TopMenuBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      currentPage : ""
+    }
+  }
+
+  onClickTitle = (e)=>{
+    console.log(e.target.getAttribute('name'));
+  }
+  render(){
+    return (
+          <div className="App">
+            <header className="App-header">
+              <div className="header-title" name="home" onClick={this.onClickTitle}><label>Home</label></div>
+              <div className="header-title" name="about" onClick={this.onClickTitle}><label>About</label></div>
+              <div className="header-title" name="skills" onClick={this.onClickTitle}><label>My Skills</label></div>
+              <div className="header-title" name="portfolio" onClick={this.onClickTitle}><label>PortFolio</label></div>
+              <div className="header-title" name="contact" onClick={this.onClickTitle}><label>Contact</label></div>
+            </header>
+          </div>
+        );
+  }
 }
+// function App(){
+//   contructor(props){
+//     super(props);
+
+//   }
+//   onClickTopMenu = () =>{
+//     alert();
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div className="header-title"><label>Home</label></div>
+//         <div className="header-title"><label>About</label></div>
+//         <div className="header-title"><label>My Skills</label></div>
+//         <div className="header-title"><label>PortFolio</label></div>
+//         <div className="header-title"><label>Contact</label></div>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
